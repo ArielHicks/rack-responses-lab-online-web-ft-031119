7d9877f3140831d4670d7f1174ad971438988d03
Ariel Hicks <hicks.ariel@gmail.com>
2019-04-18 18:21:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,15 @@
-class App
 
-  def call(env)
-    resp = Rack::Response.new
+class Application
 
-   num_1 = Kernel.rand(1..20)
-   num_2 = Kernel.rand(1..20)
-   num_3 = Kernel.rand(1..20)
-
-   resp.write "#{num_1}\n"
-   resp.write "#{num_2}\n"
-   resp.write "#{num_3}\n"
-
-   if num_1==num_2 && num_2==num_3
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
-    resp.finish
-  end
-
+def call(env)
+resp = Rack::Response.new
+now = Time.now
+if now.hour >=12
+  resp.write "Good Afternoon"
+else
+  resp.write "Good Morning"
+end
+resp.finish
+end
 
 end
